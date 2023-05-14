38f3282c01ff4ef8ba0a2331b0d6670373fbc8cd
JorgeEmi <mendoza.bautista.jorge.emilio@gmail.com>
2023-05-13 21:45:43 -0600

    primer cambio

---
+++ firebaseconfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCPCIqpi9DgNNWJO3fS2YGQGkWHo4XlEbw",
+  authDomain: "bustracker-e6f0b.firebaseapp.com",
+  projectId: "bustracker-e6f0b",
+  storageBucket: "bustracker-e6f0b.appspot.com",
+  messagingSenderId: "561049088786",
+  appId: "1:561049088786:web:c3f9e2998503778579f9cb",
+  measurementId: "G-W8R8L9ZDHZ"
+};
+
